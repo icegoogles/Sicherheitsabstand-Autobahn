fedfe6c2e8624d6b8f0536cfcf01fe5301eb125a
Nathan <icenathanvideos@gmail.com>
2025-01-21 23:35:05 +0100

    vollbremsung graph

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # Sicherheitsabstand-Autobahn
 Genug Sicherheitsabstand auf der Autobahn ist sehr wichtig. Folgende Grafik versucht den Abstand bei bestimmten Geschwindigkeiten einzuordnen.
 
-![Ergebnisse](results.svg)
+![Übersicht](overview.svg)
+
+Folgende Grafik zeigt, welcher Sicherheitsabstand nötig ist, wenn zwei Autos die gleiche Geschwindigkeit haben und das vordere Auto eine Vollbremsung macht.
+
+![Ergebnisse](vollbremsung_auto_vorne.svg)
--- calculation_and_plot.m
+++ calculation_and_plot.m
@@ -1,7 +1,7 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Sicherheitsabstand auf der Autobahn visualisiert %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-speed = 80:10:160;  % Geschwindigkeitsbereich [km/h]
+speed = 80:10:250;  % Geschwindigkeitsbereich [km/h]
 
 %% Bußgeld in Deutschland
 % Quelle Bußgeld: https://www.bussgeldkatalog.org/abstand/ (Stand: 21.01.2025)
@@ -32,7 +32,7 @@ stop_distance_0s = ...
 %% Plot
 figure(1); clf; grid on; hold on;
 xlabel("Geschwindigkeit [km/h]"); ylabel("Abstand [m]");
-ylim([0, 200]);
+ylim([0, 200]); xlim([80, 250]);
 
 plot(speed, v_2, "b", "LineWidth",2)
 plot(speed, v_4, ".-")
@@ -52,4 +52,39 @@ legend([...
     ],...
     "Location","northwest")
 
-set(gcf,'units','centimeters','position',[5,5, 12,18]);  % set size
+set(gcf,'units','centimeters','position',[5,5, 22,18]);  % set size
+
+saveas(gcf, 'overview.svg');  % save as image
+
+%% Plot2 Vorderes Auto macht Vollbremsung (Abstand bei Stillstand)
+
+distance_after_80 = 80*ones(size(speed)) + stop_distance_0s - stop_distance_1s;
+distance_after_60 = 60*ones(size(speed)) + stop_distance_0s - stop_distance_1s;
+distance_after_40 = 40*ones(size(speed)) + stop_distance_0s - stop_distance_1s;
+distance_after_20 = 20*ones(size(speed)) + stop_distance_0s - stop_distance_1s;
+
+
+figure(2); clf; grid on; hold on;
+xlabel("Geschwindigkeit [km/h]"); ylabel("Abstand nach Vollbremsung [m]");
+xlim([80, 250]); ylim([-20, max(distance_after_80)]);
+title("Vollbremsung vom vorrausfahrenden Auto");
+
+rectangle('Position',[0,-100,300,100],'FaceColor',[255/255, 176/255, 176/255],'LineWidth',0.001)
+
+plot(speed, distance_after_80, "LineWidth",2)
+plot(speed, distance_after_60, "LineWidth",2)
+plot(speed, distance_after_40, "LineWidth",2)
+plot(speed, distance_after_20, "LineWidth",2)
+
+yline(0, "k", "Unfall-Linie", "LineWidth",2)
+xline(130, "k--")  % Richtgeschwindigkeit
+
+legend([...
+    "80m Sicherheitsabstand", ...
+    "60m Sicherheitsabstand", ...
+    "40m Sicherheitsabstand",...
+    "20m Sicherheitsabstand",...
+    ],...
+    "Location","northeast")
+
+saveas(gcf, 'vollbremsung_auto_vorne.svg');  % save as image
