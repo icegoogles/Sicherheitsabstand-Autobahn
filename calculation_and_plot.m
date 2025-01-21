8eb7c5aa5641fbd7899381c90a799f9cefd42eaa
Nathan <icenathanvideos@gmail.com>
2025-01-21 22:01:06 +0100

    formatting

--- calculation_and_plot.m
+++ calculation_and_plot.m
@@ -1,14 +1,16 @@
-% Sicherheitsabstand
-speed = 80:10:160;  % Geschwindigkeit [km/h]
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Sicherheitsabstand auf der Autobahn visualisiert %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+speed = 80:10:160;  % Geschwindigkeitsbereich [km/h]
 
-% Bußgeld in Deutschland
+%% Bußgeld in Deutschland
 % Quelle Bußgeld: https://www.bussgeldkatalog.org/abstand/ (Stand: 21.01.2025)
 
-v_2 = speed./2;          % halber Tacho
-v_4 = v_2.*(5/10);       % viertel Tacho (Start Bußgeld)
+v_2 = speed./2;          % Halber Tacho
+v_4 = v_2.*(5/10);       % Viertel Tacho (Start Bußgeld)
 v_2_3_10 = v_2.*(3/10);  % Start Fahrverbot (wenn über 100km/h)
 
-% Anhalteweg
+%% Anhalteweg
 % Quelle Beschleunigungswert: https://copradar.com/chapts/references/acceleration.html
 % "Many safety experts use 15 ft/sec2 (0.47 g's) as the maximum deceleration
 % that is safe for the average driver to maintain control"
@@ -27,7 +29,7 @@ stop_distance_0s = ...
     speed_meter_per_second .* reaction_time + ...
     speed_meter_per_second.^2 ./ (2*breaking_acceleration);
 
-% Plot
+%% Plot
 figure(1); clf; grid on; hold on;
 xlabel("Geschwindigkeit [km/h]"); ylabel("Abstand [m]");
 
